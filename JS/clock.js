bf455799cfa264ac8eaf06e9a495e19366897589
rainyel97 <85095908+rainyel97@users.noreply.github.com>
2022-08-11 16:14:33 +0900

    6.1

--- JS/clock.js
+++ JS/clock.js
@@ -1,24 +1,25 @@
 const clock = document.querySelector("h2#clock");
-//시간단위 변수 설정하기
-const day = 24 * 60 * 60 * 1000;
-const hour = 60 * 60 * 1000;
-const minute = 60 * 1000;
-const second = 1000;
 
 function showTime() {
   const date = new Date();
-  const christmas = new Date(2022, 11, 25); //월은 0부터 시작함
-  const remainTime = christmas - date; //Time difference
 
-  const remainDay = String(Math.floor(remainTime / day));
-  const remainHour = String(Math.floor((remainTime % day) / hour));
-  const remainMinute = String(Math.floor((remainTime % hour) / minute));
-  const remainSecond = String(Math.floor((remainTime % minute) / second));
-
-  clock.innerText = `${remainDay.padStart(2, "0")}d ${remainHour.padStart(
-    2,
-    "0"
-  )}h ${remainMinute.padStart(2, "0")}m ${remainSecond.padStart(2, "0")}s`;
+  if (date.getHours() < 13) {
+    const Hour = String(date.getHours());
+    const Minute = String(date.getMinutes());
+    const Second = String(date.getSeconds());
+    clock.innerText = `오전 ${Hour}시 ${Minute.padStart(
+      2,
+      "0"
+    )}분 ${Second.padStart(2, "0")}초`;
+  } else {
+    const Hour = String(date.getHours() % 12);
+    const Minute = String(date.getMinutes());
+    const Second = String(date.getSeconds());
+    clock.innerText = `오후 ${Hour}시 ${Minute.padStart(
+      2,
+      "0"
+    )}분 ${Second.padStart(2, "0")}초`;
+  }
 }
 
 showTime();
