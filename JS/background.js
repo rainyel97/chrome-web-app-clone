fdb0af07631725f300d323d9f1239c0964ffbc02
rainyel97 <85095908+rainyel97@users.noreply.github.com>
2022-08-12 11:37:44 +0900

    1

--- JS/background.js
+++ JS/background.js
@@ -2,6 +2,6 @@ const images = ["back1.png", "back2.png"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `Img/${chosenImage}`;
 bgImage.classList.add("back");
 document.body.appendChild(bgImage);
